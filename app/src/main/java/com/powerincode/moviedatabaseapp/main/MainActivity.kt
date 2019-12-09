9181bca2e430e49002254ce96ed3f94133f5e092
Yaroslav Trubnikov <yaroslav.trubnikov@revolut.com>
2019-12-09 13:26:02 +0300

    Initialize app structure

---
+++ app/src/main/java/com/powerincode/moviedatabaseapp/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.powerincode.moviedatabaseapp.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.powerincode.moviedatabaseapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

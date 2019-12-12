ef57f4b72f59326e63c9a569123a02d6d294afc9
Yaroslav Trubnikov <yaroslav.trubnikov@revolut.com>
2019-12-12 14:12:02 +0300

    Upload architecture

--- app/src/main/java/com/powerincode/moviedatabaseapp/main/MainActivity.kt
+++ app/src/main/java/com/powerincode/moviedatabaseapp/main/MainActivity.kt
@@ -1,32 +1,18 @@
 package com.powerincode.moviedatabaseapp.main
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
-import com.powerincode.core.domain.repositories.Data
 import com.powerincode.core.ui.activity.BaseActivity
-import com.powerincode.domain.repositories.MovieRepository
 import com.powerincode.moviedatabaseapp.R
-import com.powerincode.moviedatabaseapp.extensions.extractData
 import com.powerincode.moviedatabaseapp.extensions.getApplicationComponent
 import com.powerincode.moviedatabaseapp.main.viewmodel.MainViewModel
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MainActivity : BaseActivity(R.layout.activity_main) {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
-
-    @Inject
-    lateinit var repository: MovieRepository
 
     override fun inject() {
         getApplicationComponent().mainActivityComponent().create(this).inject(this)
@@ -37,57 +23,5 @@ class MainActivity : BaseActivity(R.layout.activity_main) {
         super.onCreate(savedInstanceState)
 
         val vm = ViewModelProviders.of(this, viewModelFactory)[MainViewModel::class.java]
-
-        launch {
-            repository.getPopularMovies(null)
-                .onEach {
-                    progressBar.visibility = if (it is Data.LOADING) View.VISIBLE else View.GONE
-                }
-                .filter { it !is Data.LOADING }
-                .catch {
-                    progressBar.visibility = View.GONE
-                    Toast.makeText(this@MainActivity, it.message, Toast.LENGTH_SHORT).show()
-                }
-                .extractData()
-                .collect {
-                    repository.getPopularMovies(true)
-                        .onEach {
-                            progressBar.visibility = if (it is Data.LOADING) View.VISIBLE else View.GONE
-                        }
-                        .filter { it !is Data.LOADING }
-                        .catch {
-                            progressBar.visibility = View.GONE
-                            Toast.makeText(this@MainActivity, it.message, Toast.LENGTH_SHORT).show()
-                        }
-                        .extractData()
-                        .collect {
-                            repository.getPopularMovies(false)
-                                .onEach {
-                                    progressBar.visibility = if (it is Data.LOADING) View.VISIBLE else View.GONE
-                                }
-                                .filter { it !is Data.LOADING }
-                                .catch {
-                                    progressBar.visibility = View.GONE
-                                    Toast.makeText(this@MainActivity, it.message, Toast.LENGTH_SHORT).show()
-                                }
-                                .extractData()
-                                .collect {
-                                    repository.getPopularMovies(null)
-                                        .onEach {
-                                            progressBar.visibility = if (it is Data.LOADING) View.VISIBLE else View.GONE
-                                        }
-                                        .filter { it !is Data.LOADING }
-                                        .catch {
-                                            progressBar.visibility = View.GONE
-                                            Toast.makeText(this@MainActivity, it.message, Toast.LENGTH_SHORT).show()
-                                        }
-                                        .extractData()
-                                        .collect {
-
-                                        }
-                                }
-                        }
-                }
-        }
     }
 }
--- app/src/main/java/com/powerincode/moviedatabaseapp/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/powerincode/moviedatabaseapp/main/viewmodel/MainViewModel.kt
@@ -3,23 +3,11 @@ package com.powerincode.moviedatabaseapp.main.viewmodel
 import com.powerincode.core.domain.viewmodels.BaseViewModel
 import com.powerincode.core.ui.dialog.progress.ProgressLoadingView
 import com.powerincode.core.ui.dialog.toast.ToastView
-import com.powerincode.domain.repositories.MovieRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import javax.inject.Inject
 
 @ExperimentalCoroutinesApi
 class MainViewModel @Inject constructor(
     private val progressDialog: ProgressLoadingView,
-    private val toastView: ToastView,
-    private val repository: MovieRepository
-) : BaseViewModel() {
-    init {
-//        launch {
-//            repository.getPopularMovies(true)
-//                .withProgress(progressDialog)
-//                .errorHandler(toastView)
-//                .safeCollect {
-//                }
-//        }
-    }
-}
+    private val toastView: ToastView
+) : BaseViewModel()
